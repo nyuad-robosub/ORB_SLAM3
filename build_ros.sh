0558a848826f7db221d5023840c0b61086eddeee
dolf321 <dolfzigler@hotmail.com>
2022-06-02 12:35:27 +0400

    Added new oak d code that supports orb slam 3

--- build_ros.sh
+++ build_ros.sh
@@ -1,6 +1,6 @@
 echo "Building ROS nodes"
-cd ./Examples_old/ROS/ORB_SLAM3
+cd ./Examples/ROS/ORB_SLAM3
 mkdir build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release
-make -j
+cmake .. -DROS_BUILD_TYPE=Release -D'depthai_DIR=/home/rami/nr22-software/include/depthai-core/build'
+make
