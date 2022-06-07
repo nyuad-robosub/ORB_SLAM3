ff94008bbd982a18962f9812e265904939f0b1b7
dolf321 <dolfzigler@hotmail.com>
2022-06-07 21:55:37 +0400

    Added launch file to run both ros oakd node and the depthai-ros node, also created Params class to get the parameters of the launch file

--- Examples/ROS/ORB_SLAM3/src/OAKD/ros_stereo_oakd.cc
+++ Examples/ROS/ORB_SLAM3/src/OAKD/ros_stereo_oakd.cc
@@ -18,6 +18,7 @@
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <image_transport/image_transport.h> //used for cv images
+#include <image_transport/subscriber_filter.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/mat.hpp>
@@ -48,59 +49,86 @@ typedef struct _Pose
     Eigen::Vector3f position;
     Eigen::Quaternionf rotation;
 } Pose;
+class Params {
+protected:
+    ros::NodeHandle nh_;
+    std::string vocab, config, left_img_name, right_img_name;
+    void ReadParams() {
+        int badParams = 0;
+        badParams += !nh_.getParam("/Stereo_oakd/vocab", vocab);
+        badParams += !nh_.getParam("/Stereo_oakd/config", config);
+        cout << badParams;
+        badParams += !nh_.getParam("/Stereo_oakd/left_img_name", left_img_name);
+        cout << badParams;
+        badParams += !nh_.getParam("/Stereo_oakd/right_img_name", right_img_name);//what about framerates?
+        cout << badParams;
 
-class Handler
+        if (badParams > 0)
+        {
+            ROS_ERROR("Couldn't find at least one of the parameters");
+        }
+        /*vocab="/home/rami/nr22-software/include/ORB_SLAM3_OPENCV4/Vocabulary/ORBvoc.txt";
+        config="/home/rami/nr22-software/include/ORB_SLAM3_OPENCV4/Examples/ROS/ORB_SLAM3/Asus1.yaml";
+        left_img_name="/stereo_publisher/left/image";
+        right_img_name="/stereo_publisher/right/image";*/
+    }
+public: 
+    Params() {
+        ReadParams();
+    }
+};
+class Handler : public Params
 {
 private:
     ORB_SLAM3::System SLAM;
     tf2_ros::TransformBroadcaster tfb;
-    ros::NodeHandle nh_;
     image_transport::ImageTransport it_;
     typedef image_transport::SubscriberFilter ImageSubscriber;
     ImageSubscriber left_img, right_img, disparity_img;
 
     typedef message_filters::sync_policies::ApproximateTime<
-        sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::Image>
+        sensor_msgs::Image, sensor_msgs::Image>
         MySyncPolicy;
     message_filters::Synchronizer<MySyncPolicy> sync;
 
-    cv_bridge::CvImageConstPtr left_img_ptr,right_img_ptr;
+    cv_bridge::CvImageConstPtr left_img_ptr, right_img_ptr;
     double frame_timestamp_s;
     Pose pose;
 
 public:
-    Handler(char * vocabulary, char * config) : it_(nh_), left_img(it_, "image/left", 1), right_img(it_, "image/right", 1), sync(MySyncPolicy(10), left_img, right_img)
-    {
-        SLAM = ORB_SLAM3::System(vocabulary,config,ORB_SLAM3::System::STEREO, true);
-        sync.registerCallback(boost::bind(&MyClass::callback, this, _1, _2));
-        tf2_ros::TransformBroadcaster tfb;
+    Handler() : Params(), it_(nh_), left_img(it_, left_img_name, 1), right_img(it_, right_img_name, 1), sync(MySyncPolicy(10), left_img, right_img), SLAM(vocab, config, ORB_SLAM3::System::STEREO, true)
+    {  
+        sync.registerCallback(boost::bind(&Handler::callback, this, _1, _2));
     }
     void callback(
         const sensor_msgs::ImageConstPtr &left_img,
-        const sensor_msgs::ImageConstPtr &right_im,)
+        const sensor_msgs::ImageConstPtr &right_im)
     {
-        
+
         try
         {
             left_img_ptr = cv_bridge::toCvShare(left_img); // use same encoding of left_img also how to store cv mat
             right_img_ptr = cv_bridge::toCvShare(right_im);
             std_msgs::Header h = left_img_ptr->header;
-            frame_timestamp_s=h.stamp.sec;
+            frame_timestamp_s = h.stamp.sec;
+
+            Sophus::SE3f pose_sophus = SLAM.TrackStereo(
+                left_img_ptr->image, // CV:MAT USE CV BRIDGE TO GET IT 8UC1 CV MAT
+                right_img_ptr->image,
+                frame_timestamp_s);
+            pose.position = pose_sophus.translation();
+            pose.rotation = pose_sophus.unit_quaternion();
+            PublishTF(pose);
         }
         catch (cv_bridge::Exception &e)
         {
             ROS_ERROR("cv_bridge exception: %s", e.what());
             return;
         }
-
-        Sophus::SE3f pose_sophus = SLAM.TrackStereo(
-            left_img_ptr->image, // CV:MAT USE CV BRIDGE TO GET IT 8UC1 CV MAT
-            right_img_ptr->image,
-            frame_timestamp_s);
-        pose.position = pose_sophus.translation();
-        pose.rotation = pose_sophus.unit_quaternion();
-        PublishTF(pose);
-        
+        catch(...) {
+            ROS_ERROR("An unknown exception occured.");
+            return;
+        }
     }
 
     void PublishTF(Pose &ps)
@@ -152,39 +180,23 @@ cv::Mat imgframe_to_mat(std::shared_ptr<dai::ImgFrame> frame, int data_type = CV
  * MAIN
  * ------------------------------------------------------------------------- */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    typedef image_transport::SubscriberFilter ImageSubscriber;
 
     // get depthai left and right camera feed to orb slam then take orb slam 2 point cloud and tf2
 
     // CREATE THREAD FOR LOOP THAT RUNS ORB SLAM 2 PUBLISH DATA THROUGH USING MUTEX AND ACCESSING IT
     // CREATE LOOP THAT RUNS ORB SLAM 2 THEN PUBLISH THE DATA USING PUBLISH
-    ros::init(argc, argv, "STEREO_OAKD");
-    ros::start();
+    ros::init(argc, argv, "Stereo_oakd");
 
-    if (argc != 3)
+    // ros::start();
+
+    /*if (argc != 3)
     {
         cout << argc << endl;
         ROS_WARN("Arguments supplied via command line are neglected.");
-    }
-    //ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::STEREO, true);
-    Handler hdl(argv[1],argv[2]);
-    ros::NodeHandle node_handle;
-    // ros::Publisher PC = node_handle.advertise<sensor_msgs::PointCloud2>("ORBSLAM2_PC2", 1);
-    // ros::Publisher pose_publisher_ = node_handle.advertise<geometry_msgs::PoseStamped>("ORBSLAM2_POSE", 1);
-    /*tf2_ros::TransformBroadcaster tfb;
-
-    ImageSubscriber ImageSubscriber;
-
-    typedef message_filters::sync_policies::ApproximateTime<
-        sensor_msgs::Image, sensor_msgs::Image>
-        MySyncPolicy;
-
-    message_filters::Synchronizer<MySyncPolicy> sync;*/
-
-    // sync.registerCallback(boost::bind(&MyClass::callback, this, _1, _2));
-
+    }*/
+    Handler hdl;
     // Stop all SLAM threads
 
     ros::spin();
