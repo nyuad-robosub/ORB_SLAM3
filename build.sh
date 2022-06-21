264eaa716bfdab0fd721f3e7b668674898ce39d7
dolf321 <dolfzigler@hotmail.com>
2022-06-21 12:47:03 +0400

    Added opencv support ot outside dependencies

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
-cd Thirdparty
-wget -O opencv.zip https://github.com/opencv/opencv/archive/refs/tags/4.4.0.zip
-unzip opencv
-rm opencv.zip
-
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd ./DBoW2
@@ -10,14 +5,6 @@ mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
-
-cd ../../opencv-4.4.0
-
-echo "Configuring and building Thirdpart/opencv"
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
 
 cd ../../g2o
 echo "Configuring and building Thirdparty/g2o ..."
